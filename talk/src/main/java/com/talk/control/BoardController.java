5cc1660e907a53ab9285fe870cd1f0b36edb8fec
Fi2a <fia_to@naver.com>
2025-04-04 16:02:04 +0900

    0404

--- talk/src/main/java/com/talk/control/BoardController.java
+++ talk/src/main/java/com/talk/control/BoardController.java
@@ -1,5 +1,7 @@
 package com.talk.control;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.talk.Dto.BoardDetailDto;
 import com.talk.Dto.BoardDto;
+import com.talk.Dto.BoardListDto;
 import com.talk.Dto.CommentDto;
+import com.talk.Dto.CommentViewDto;
 import com.talk.service.BoardService;
 
 import jakarta.servlet.http.HttpSession;
@@ -26,8 +31,23 @@ public class BoardController {
 	@GetMapping("/index")
 	public String boardIndex(@RequestParam("page") int pageNum,	Model model) {
 		
+		// 한 페이지에 보여줄 게시글 가져오기
+		List<BoardListDto> boardListDtos = boardService.boardList(pageNum);
+		model.addAttribute("boardListDtos" , boardListDtos);
+	
+		// 페이징을 위해 필요한 전체 게시글 갯수 가져오기
+		int totalCount = boardService.boardCount();
+		int totalPage = totalCount % 10 == 0 ? totalCount/10 : totalCount/10+1;
+		int start = pageNum>5 ? pageNum-5 : 1 ;
+		int end = start+9 > totalPage ? totalPage : start+9;
+		
+		model.addAttribute("page", pageNum);
+		model.addAttribute("start", start);
+		model.addAttribute("end", end);
+		
 		return "board/boardList";
 	}
+	
 	//글 페이지
 	@GetMapping("/write")
 	public String  boardWrite(HttpSession session,	Model model) {
@@ -60,14 +80,24 @@ public class BoardController {
 	}
 	//수정
 	@GetMapping("/update")
-	public String  boardDetail(@RequestParam("id")	int id ,Model model) {
+	public String  boardUpdate(@RequestParam("id")	int id ,Model model) {
 		return null;
 	}
 	//상세
 	@GetMapping("/detail")
-	public String  boardUpdate(@RequestParam("id")	int id,	HttpSession session , 
+	public String  boardDetail(@RequestParam("id")	int id,	HttpSession session , 
 	Model model) {
-		return null;
+		
+		BoardDetailDto boardDetailDto = boardService.boardDetail(id);
+
+		model.addAttribute("boardDetailDto", boardDetailDto);
+		CommentDto commentDto = new CommentDto();
+		commentDto.setBoardId(id);
+		model.addAttribute("commentDto", commentDto);
+		
+		boardService.boardDetail(id);
+		
+		return "board/boardDetail";
 	}
 	//댓글 저장
 	@GetMapping("/commentSave")
